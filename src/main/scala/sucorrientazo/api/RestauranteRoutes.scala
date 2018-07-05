d6626a2cbc7486d588c5f244418397f445450b68
Samuel Gomez Ramos <samuelgomez@seven4n.com>
2018-07-04 20:22:51 -0500

    se habilita el codigo que simula fallos

--- src/main/scala/sucorrientazo/api/RestauranteRoutes.scala
+++ src/main/scala/sucorrientazo/api/RestauranteRoutes.scala
@@ -18,6 +18,7 @@ import sucorrientazo.actors.Service.ObtenerDirecciones
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.util.Random
 
 trait RestauranteRoutes extends RestauranteMarshaller {
   implicit val actorSys: ActorSystem
@@ -45,50 +46,36 @@ trait RestauranteRoutes extends RestauranteMarshaller {
           onClose(println("circuit breaker closed!")).
           onHalfOpen(println("circuit breaker half-open"))
 
-      /*(1 to 100).map(x => {
+      (1 to 100).map(x => {
         Thread.sleep(50)
         val random = Random.nextInt(3)
         println("random = " + random)
         val fallaAveces: Future[HttpResponse] = if (random == 2) {
           response
-          // convertir este json a una lista de direcciones de almuerzos
-          // llamar al actor que reparte los almuerzos
-          // almacenar en base de datos los almuerzos entregados
         } else {
           Future.failed(new Exception("Falla aveces"))
-        } */
+        }
 
-      val askFuture: Future[HttpResponse] = breaker.withCircuitBreaker(response)
+        val askFuture: Future[HttpResponse] = breaker.withCircuitBreaker(fallaAveces)
 
-      val fAlmuerzos: Future[String] = askFuture.map {
-        httpResponse =>
-          val entity = httpResponse.entity.asInstanceOf[HttpEntity.Strict]
-          val entidad = entity.data.utf8String
-          val json: JsObject = Json.parse(entidad).as[JsObject]
+        val fAlmuerzos: Future[String] = askFuture.map {
+          httpResponse =>
+            val entity = httpResponse.entity.asInstanceOf[HttpEntity.Strict]
+            val entidad = entity.data.utf8String
+            val json: JsObject = Json.parse(entidad).as[JsObject]
+            val almuerzoMapper: AlmuerzosMapper = transformarJson(json)
+            entregaActor ! EntregarListado(almuerzoMapper)
+            "Se ha iniciado el proceso, por favor consulte el log"
+        }
 
-          //json.as[Almuerzos]
-          val almuerzoMapper: AlmuerzosMapper = transformarJson(json)
-          entregaActor ! EntregarListado(almuerzoMapper)
-          "Se ha iniciado el proceso, por favor consulte el log"
-      }
-
-      /*fAlmuerzos.map {
-        m =>
-          println("resultado del futuro ===============>" + m)
-      }.recover({
-        case t => "error: " + println(t.toString)
-      }) */
-      //})
-
-      // entregaActor ! "prueba"
-      //val respuesta: Future[String] = (entregaActor ? "prueba").mapTo[String]
-
-      onSuccess(fAlmuerzos) { performed =>
-        complete((StatusCodes.Created, "Esta es la respuesta ===============> " + performed))
-      }
-
-      /*Thread.sleep(50000)
-      complete("ok")*/
+        fAlmuerzos.map {
+          m =>
+            println("resultado del futuro ===============>" + m)
+        }.recover({
+          case t => "error: " + println(t.toString)
+        })
+      })
+      complete("ok")
 
     }
   }
